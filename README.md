b038af31acba7711e8694b2b6198a3f7087b8a5f
Rahul Batra <rahulbatra@d-172-26-32-84.dhcp.virginia.edu>
2017-10-29 15:55:36 -0400

    added script and made a readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Auto-Downloader-Userscript
 Automatically download files from file-sharing websites
+
+- Basically skips the extra clicking needed when downloading stuff
+- Currently supports zippyshare, filemack, and suprafiles.
+  - Will add more support soon. Probably dbr.ee first
+
+
+# Installation
+1. To install this script get Tampermonkey (chrome) or another UserScript extension for your broswer
+2. Install script from Greasyfork.org so that you always have the latest version -
---
+++ script.user.js
@@ -0,0 +1,79 @@
+// ==UserScript==
+// @name         Download Link Skipper (suprafiles, zippyshare, filemack)
+// @namespace    http://tampermonkey.net/
+// @version      0.5
+// @description  Skips the extra clicking needed when downloading stuff
+// @author       spyruf
+// @include      http://*suprafiles.*/*
+// @include      https://*filemack.com/*
+// @include      http://*zippyshare.com/*
+// @grant        none
+// @require http://code.jquery.com/jquery-latest.js
+
+// ==/UserScript==
+
+if (window.top != window.self)  //-- Don't run on frames or iframes
+    return;
+
+
+(function() {
+    'use strict';
+
+    console.log("window.location.href is: " + window.location.href);
+    console.log(" Link Skipper is running!");
+
+    if (window.location.href.indexOf("zs_") != -1 ){
+
+
+        var s = $(':submit').each(function() {
+            (this).click();
+        });
+
+        //console.log("you're on submit");
+
+    }
+
+    else if (window.location.href.indexOf("filemack") != -1 ){
+
+        //selects the element that has the link -- jquery obj version
+        var f = $('a.truncate[href*="zs_"]').each(function() {
+
+        });
+
+
+        //f.css( "text-decoration", "bold" );
+
+        var fValue = f.attr("href");
+
+        //console.log("Link Address filemack : " + fValue);
+
+        window.open(fValue,"_self");
+
+        //console.log("Still running filemack : " + fValue);
+
+
+    }
+
+    else if (window.location.href.indexOf("zippyshare") != -1 ){
+
+        //console.log("boutta cop");
+
+        $('#dlbutton')[0].click();
+
+
+        localStorage.setItem('download', 'false');
+
+
+    }
+
+    else if (window.location.href.indexOf("suprafiles") != -1 ){
+
+        var supraSubmit = $(':submit').each(function() {
+            (this).click();
+        });
+
+        $('a:contains("fs")')[0].click();
+
+    }
+
+})();
