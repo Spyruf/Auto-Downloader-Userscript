bd0ad5846cb4d7ab66d76d0bc0849c5a5fde332a
Rahul Batra <spyruf@gmail.com>
2017-12-17 00:33:46 -0500

    added support for dbree

--- script.user.js
+++ script.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Download Link Skipper (suprafiles, zippyshare, filemack)
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Skips the extra clicking needed when downloading stuff
 // @author       spyruf
 // @include      http://*suprafiles.*/*
 // @include      https://*filemack.com/*
 // @include      http://*zippyshare.com/*
+// @include      http://dbr.ee/*
 // @grant        none
 // @require http://code.jquery.com/jquery-latest.js
 
@@ -16,68 +17,66 @@
 
 // TEST 1 TEST 2 TEST 3
 
-if (window.top != window.self)  //-- Don't run on frames or iframes
-    return;
+if (window.top != window.self) //-- Don't run on frames or iframes
+  return;
 
 
 (function() {
-    'use strict';
+  'use strict';
 
-    console.log("window.location.href is: " + window.location.href);
-    console.log(" Link Skipper is running!");
+  console.log("window.location.href is: " + window.location.href);
+  console.log(" Link Skipper is running!");
 
-    if (window.location.href.indexOf("zs_") != -1 ){
-
-
-        var s = $(':submit').each(function() {
-            (this).click();
-        });
-
-        //console.log("you're on submit");
-
-    }
-
-    else if (window.location.href.indexOf("filemack") != -1 ){
-
-        //selects the element that has the link -- jquery obj version
-        var f = $('a.truncate[href*="zs_"]').each(function() {
-
-        });
+  if (window.location.href.indexOf("zs_") != -1) {
 
 
-        //f.css( "text-decoration", "bold" );
+    var s = $(':submit').each(function() {
+      (this).click();
+    });
 
-        var fValue = f.attr("href");
+    //console.log("you're on submit");
 
-        //console.log("Link Address filemack : " + fValue);
+  } else if (window.location.href.indexOf("filemack") != -1) {
 
-        window.open(fValue,"_self");
+    //selects the element that has the link -- jquery obj version
+    var f = $('a.truncate[href*="zs_"]').each(function() {
 
-        //console.log("Still running filemack : " + fValue);
-
-
-    }
-
-    else if (window.location.href.indexOf("zippyshare") != -1 ){
-
-        //console.log("boutta cop");
-
-        $('#dlbutton')[0].click();
+    });
 
 
-        localStorage.setItem('download', 'false');
+    //f.css( "text-decoration", "bold" );
+
+    var fValue = f.attr("href");
+
+    //console.log("Link Address filemack : " + fValue);
+
+    window.open(fValue, "_self");
+
+    //console.log("Still running filemack : " + fValue);
 
 
-    }
+  } else if (window.location.href.indexOf("zippyshare") != -1) {
 
-    else if (window.location.href.indexOf("suprafiles") != -1 ){
+    //console.log("boutta cop");
 
-        var supraSubmit = $(':submit').each(function() {
-            (this).click();
-        });
+    $('#dlbutton')[0].click();
 
-        $('a:contains("fs")')[0].click();
 
-    }
+    localStorage.setItem('download', 'false');
+
+
+  } else if (window.location.href.indexOf("suprafiles") != -1) {
+
+    var supraSubmit = $(':submit').each(function() {
+      (this).click();
+    });
+
+    $('a:contains("fs")')[0].click();
+
+  } else if (window.location.href.indexOf("dbr") != -1) {
+
+    $('#download_btn')[0].click();
+
+  }
 
 })();
