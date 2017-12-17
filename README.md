128b4eafac767acd355b8620319351df1375cfe7
Rahul Batra <spyruf@gmail.com>
2017-12-17 01:32:06 -0500

    fixed dbree, and did some 9clacks stuff

--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 Automatically download files from file-sharing websites
 
 - Basically skips the extra clicking needed when downloading stuff
-- Currently supports zippyshare, filemack, and suprafiles.
-  - Will add more support soon. Probably dbr.ee first
-
+- Currently supports zippyshare, filemack, dbree, srcleaks, and some other stuff!
 
 # Installation
 1. To install this script get Tampermonkey (chrome) or another UserScript extension for your broswer
--- script.user.js
+++ script.user.js
@@ -1,9 +1,11 @@
 // ==UserScript==
-// @name         Download Link Skipper (suprafiles, zippyshare, filemack)
+// @name         Download Link Skipper (suprafiles, zippyshare, filemack, dbree, cloudyfiles, 9clacks)
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Skips the extra clicking needed when downloading stuff
 // @author       spyruf
+// @include      *9clacks*.*
+// @include      *srcleaks*.*
 // @include      *suprafiles.*/*
 // @include      *cloudyfiles.*/*
 // @include      *filemack.com/*
@@ -15,9 +17,6 @@
 // ==/UserScript==
 
 
-
-// TEST 1 TEST 2 TEST 3
-
 if (window.top != window.self) //-- Don't run on frames or iframes
   return;
 
@@ -26,16 +25,69 @@ if (window.top != window.self) //-- Don't run on frames or iframes
   'use strict';
 
   console.log("window.location.href is: " + window.location.href);
-  console.log(" Link Skipper is running!");
+  console.log("Link Skipper is running!");
+  console.log("localStorage variable download is: " + localStorage.getItem("download"));
 
-  if (window.location.href.indexOf("zs_") != -1) {
+  // 9clacks stuff, ignore for most ppl
+
+  if (localStorage.getItem("download") === null) {
+
+    //console.log("settinglocalstorage");
+
+    localStorage.setItem('download', 'null');
+  }
+
+  if (window.location.href.indexOf("9clacks") != -1) {
+
+    console.log("I am on 9clacks page");
+
+    $('a:contains("Read More")').click(
+      function() {
+
+        console.log("Read More clicked, proceeding to download");
+
+        localStorage.setItem('download', 'true');
+
+      });
+  }
+  if (window.location.href.indexOf("9clacks2") != -1 && localStorage.getItem("download") == 'true') {
+
+    console.log("finding Link");
+
+    var link = $('a:contains("cloudyfiles")').attr('href');
+    link = link.slice(link.indexOf("=") + 1);
+    console.log("Link is: " + link);
 
 
-    var s = $(':submit').each(function() {
-      (this).click();
-    });
+    localStorage.setItem('download', 'false');
+    window.history.back();
+    window.open(link, "_blank");
 
-    //console.log("you're on submit");
+  } else if (window.location.href.indexOf("srcleaks") != -1) {
+
+    console.log("getting dl link");
+
+    // First try dbree
+    // Then suprafiles
+    // Then any iTunes DL
+    // Then any at all
+
+    //iTunes
+    if ($('a:contains("iTunes DL"):contains("dbree")')[0] !== null)
+      $('a:contains("iTunes DL"):contains("dbree")')[0].click();
+    else if ($('a:contains("iTunes DL"):contains("suprafiles")')[0] !== null)
+      $('a:contains("iTunes DL"):contains("suprafiles")')[0].click();
+    else if ($('a:contains("iTunes")')[0] !== null)
+      $('a:contains("iTunes")')[0].click();
+
+    //320
+    else if ($('a:contains("320 DL"):contains("dbree")')[0] !== null)
+      $('a:contains("320 DL"):contains("dbree")')[0].click();
+    else if ($('a:contains("320 DL"):contains("suprafiles")')[0] !== null)
+      $('a:contains("320 DL"):contains("suprafiles")')[0].click();
+    else if ($('a:contains("320 DL")')[0] !== null)
+      $('a:contains("320 DL")')[0].click();
+
 
   } else if (window.location.href.indexOf("filemack") != -1) {
 
@@ -76,12 +128,13 @@ if (window.top != window.self) //-- Don't run on frames or iframes
 
   } else if (window.location.href.indexOf("cloudyfiles") != -1) {
 
-    var supraSubmit = $(':submit').each(function() {
+    var cloudysubmit = $(':submit').each(function() {
       (this).click();
     });
 
     $('.btn-xs')[0].click();
 
+    alert($('a:contains("s1")'));
     $('a:contains("s1")')[0].click();
 
 
